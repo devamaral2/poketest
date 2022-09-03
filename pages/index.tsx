5b100fcb21b1f673d097b8729d2aa88b43fc7cc7
Rafael Amaral <amaral.avelar.filo@gmail.com>
2022-09-03 17:13:28 -0300

    whiout jest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,11 @@
 /* eslint-disable react/jsx-filename-extension */
-import { useEffect, React, useState } from 'react';
-import Head from 'next/head';
-import { InferGetServerSidePropsType } from 'next';
+import React, { JSXElementConstructor, useEffect, useState } from 'react';
 import axios from 'axios';
-import clientPromise from '../lib/mongodb';
 
-export async function getServerSideProps(context: any) {
-  try {
-    await clientPromise;
-    // const db = await client.db('pokemon')
-    // const pki = await db.collection('saved').Find();
-    // const pk = JSON.parse(pki)
-    // console.log(pk)
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: {
-        isConnected: true,
-      },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
-}
-
-export default function Home({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home(): JSX.Element {
   interface IPk {
-    name:string
-  }
+    name:string,
+  };
 
   const [list, setList] = useState([]);
   const pktest = async () => {
@@ -47,7 +13,7 @@ export default function Home({
     setList(pokemons.data);
     console.log(pokemons.data);
   };
-  useEffect(() => {
+ useEffect(() => {
     pktest();
   }, []);
   return (
